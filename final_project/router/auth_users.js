f52880f3385c9ea995a0c578a88de2575f34d240
Barrón <vladimir.barron@accenture.com>
2024-08-14 20:53:54 -0600

    Add routes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,25 +5,51 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (user)=>{ //returns boolean
+  const unique_user = users.filter(u => u.username === user.username);
+  return unique_user.length > 0;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (user)=>{ //returns boolean
+  const auth_user = users.filter(u => u.username === user.username && u.password === user.password);
+  return auth_user.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if(!username || !password) return res.status(400).send("Username or password not provided correctly");
+  if(authenticatedUser({ "username":username, "password":password })) {
+    const accessToken = jwt.sign({ data: username }, "access", { expiresIn: 60 * 60 });
+    req.session.authorization = { accessToken, username };
+    return res.status(200).send("User successfuly logged in!");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = books[req.params.isbn];
+  if(!book) return res.status(400).send("The ISBN doesn't exist in the database");
+  const review = req.query.review;
+  if(!review) return res.status(400).send("Send a valid review");
+
+  book["reviews"][req.user.data] = review;
+  books[req.params.isbn] = book;
+  return res.status(200).send(JSON.stringify(book));
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const book = books[req.params.isbn];
+  if(!book) return res.status(400).send("The ISBN doesn't exist in database");
+  const user = req.user.data;
+  for(let [key, value] of Object.entries(book["reviews"])) {
+    if(key === user) delete book["reviews"][key];
+  }
+  return res.status(200).send(JSON.stringify(book));
+})
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,98 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const new_user = {
+    "username" : req.body.username,
+    "password" : req.body.password
+  };
+  if(!new_user.username || !new_user.password) return res.status(400).send("Username or password not provided correctly");
+  if(isValid(new_user)) return res.status(400).send("Username already exists");
+
+  users.push(new_user);
+  res.status(200).send("User was created successfully");
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.status(200).send(JSON.stringify(books));
 });
+
+const getBooks = async () => {
+  return await axios.get('/', (req, res) => {
+    res.status(200).send(JSON.stringify(books));
+  })
+}
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const book = books[req.params.isbn];
+  if(!book) return res.status(400).send("The ISBN doesn't exist in the database");
+  res.status(200).send(JSON.stringify(book));
+});
+
+const getBookByISBN = async (isbn) => {
+  return await axios.get(`/isbn/${isbn}`, (req, res) => {
+    const book = books[req.params.isbn];
+    if(!book) return res.status(400).send("The ISBN doesn't exist in the database");
+    res.status(200).send(JSON.stringify(book));
+  });
+}
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author_books = [];
+  for(let [key, value] of Object.entries(books)) {
+    if(value["author"] === req.params.author){
+      author_books.push(value);
+    }
+  }
+  return res.status(200).send(JSON.stringify(author_books));
 });
+
+const getBookByAuthor = async (author) => {
+  return await axios.get(`/author/${author}`, (req, res) => {
+    const author_books = [];
+    for(let [key, value] of Object.entries(books)) {
+    if(value["author"] === req.params.author){
+      author_books.push(value);
+    }
+  }
+  return res.status(200).send(JSON.stringify(author_books));
+  });
+}
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title_books = [];
+  for(let [key, value] of Object.entries(books)) {
+    if(value["title"] === req.params.title) {
+      title_books.push(value);
+    }
+  }
+  return res.status(200).send(JSON.stringify(title_books));
 });
+
+const getBookByTitle = async (title) => {
+  return await axios.get(`/title/${title}`, (req, res) => {
+    const title_books = [];
+    for(let [key, value] of Object.entries(books)) {
+    if(value["title"] === req.params.title) {
+      title_books.push(value);
+    }
+  }
+  return res.status(200).send(JSON.stringify(title_books));
+  });
+}
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const review = books[req.params.isbn]["reviews"];
+  if(!review) return res.status(300).send("The ISBN doens't exist in the database");
+  res.status(200).json({"ISBN Book" : req.params.isbn, "review" : JSON.stringify(review)});
 });
 
 module.exports.general = public_users;
